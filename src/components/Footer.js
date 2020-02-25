601c48bf5accbbc0afe108422c97df70c657701f
Alan Tang <atang8013@gmail.com>
2020-02-25 18:18:33 -0500

    fixed resume with hyperlinks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import resume from "../assets/files/resume.pdf";
+import resume from "../../src/assets/files/resume.pdf";
 
 class Footer extends React.Component {
   render() {
